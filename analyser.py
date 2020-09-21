6818f1244bb1f2db2683e2eeeead704e31f6fab7
Simon Renblad <simon.renblad@outlook.com>
2020-09-21 16:16:44 +0800

    added graphing features and per month totals

--- analyser.py
+++ analyser.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+from matplotlib.dates import DateFormatter
 
 FILE_NAME = "log.txt"
 
@@ -59,6 +63,8 @@ def convert_to_numerical(data):
 
         entry['seconds'] = int(duration[1][:-1])
 
+        entry['duration'] = entry['hours'] * 60 + entry['minutes']
+
         new_data.append(entry)
 
     return new_data
@@ -101,6 +107,8 @@ def per_day_totals(data):
 
                 entry['minutes'] = remaining_min
 
+                entry['duration'] = entry['hours'] * 60 + entry['minutes']
+
         # date has yet to be added to list, add to output list
         if add:
             per_day_data.append(old_entry)
@@ -109,15 +117,49 @@ def per_day_totals(data):
 
 
 def per_month_totals(data):
-    pass
+
+    per_month_data = []
+
+    # iterate over the given data
+    for old_entry in data:
+
+        # switch variable triggered to false if month already exists in output list
+        add = True
+
+        # iterate over output list, checking if month matches existing month
+        for entry in per_day_data:
+
+            # month matching
+            if old_entry['month'] == entry['month']:
+
+                # trigger switch
+                add = False
+
+                # perform addition of times using modulo 60 to carry over minutes to hours
+                # we discard seconds for now, later iterations may also carry over seconds
+                sum_minutes = old_entry['minutes'] + entry['minutes']
+
+                carry_hour = sum_minutes // 60
+
+                remaining_min = sum_minutes % 60
+
+                # record new duration (keep in mind that 'duration' key will now be useless...)
+                entry['hours'] = old_entry['hours'] + carry_hour
+
+                entry['minutes'] = remaining_min
+
+                entry['duration'] = entry['hours'] * 60 + entry['minutes']
+
+        # date has yet to be added to list, add to output list
+        if add:
+            per_month_data.append(old_entry)
+
+    return per_month_data
 
 def seven_day_average(data):
     pass
 
 def monthly_average(data):
-    pass
-
-def sort_by_date(data):
     pass
 
 # TIME VALIDATION FUNCTIONS
@@ -135,8 +177,30 @@ def validate_usage():
 
 # PRINTING FUNCTIONS
 
-def show_graph(data):
-    pass
+def convert_to_datetime(data):
+    for entry in data:
+        entry["date"] = datetime.date(int(entry["year"]), int(entry["month"]), int(entry["day"]))
+    return data
+
+def show_graph(data, xfield, yfield):
+    x = []
+    y = []
+    for entry in data:
+        x.append(entry[xfield])
+        y.append(entry[yfield])
+
+    x = np.array(x)
+    y = np.array(y)
+    if xfield == "date":
+        fig, ax = plt.subplots()
+        formatter = DateFormatter('%d-%m-%y')
+        plt.plot_date(x, y)
+        ax.xaxis.set_major_formatter(formatter)
+    else:
+        plt.plot(x, y)
+    plt.xlabel(xfield)
+    plt.ylabel(yfield)
+    plt.show()
 
 def print_stats(data):
     for entry in data:
@@ -157,6 +221,9 @@ def analyze():
 
     # testing
     print_stats(per_day_data)
+
+    # print per day data
+    show_graph(convert_to_datetime(per_day_data), "date", "duration")
 
 analyze()
 
